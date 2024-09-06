87d64319a87c8e37335d114d78c0921dc3458920
abdillah <abdillahh1453@gmail.com>
2024-09-06 14:40:35 +0700

    add:fic later

--- migrationdb.sql
+++ migrationdb.sql
@@ -31,7 +31,7 @@ CREATE TABLE `admin` (
   `nomor` bigint(20) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `foto` text NOT NULL DEFAULT 'default.png'
+  `foto` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -44,34 +44,29 @@ INSERT INTO `admin` (`nomor`, `nama`, `email`, `foto`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `karyawan`
+-- Struktur dari tabel `penempatan`
 --
 
-CREATE TABLE `karyawan` (
+CREATE TABLE `penempatan` (
   `id` int(11) NOT NULL,
-  `nik` varchar(20) NOT NULL,
-  `nama_karyawan` varchar(50) NOT NULL,
-  `jenis_kelamin` enum('Laki-Laki','Perempuan') NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `departemen` enum('Perencanaan TI','Operasional TI','Pengembangan TI','Pelayanan TI') NOT NULL,
-  `tanggal_bergabung` date NOT NULL,
-  `foto` text NOT NULL DEFAULT 'default.png',
-  `lokasi_kerja` varchar(255) DEFAULT '-- belum terdata --',
-  `alamat` text DEFAULT '-- belum terdata --'
+  `no` int(11) NOT NULL,
+  `nama_barang` varchar(255) NOT NULL,
+  `tahun_perolehan` int(4) NOT NULL,
+  `grup` int(10) NOT NULL,
+  `kategori` int(10) NOT NULL,
+  `kelas` int(10) NOT NULL,
+  `sub_kelas` int(10) NOT NULL,
+  `nomor_urut` varchar(255) NOT NULL,
+  `kode_aset` varchar(255) NOT NULL,
+  `qr_code` varchar(255) NOT NULL,
+  `kode_telkom` varchar(255) NOT NULL,
+  `serial_number` int(255) NOT NULL,
+  `foto` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `karyawan`
+-- Dumping data untuk tabel `penempatan`
 --
-
-INSERT INTO `karyawan` (`id`, `nik`, `nama_karyawan`, `jenis_kelamin`, `email`, `departemen`, `tanggal_bergabung`, `foto`, `lokasi_kerja`, `alamat`) VALUES
-(1, '012092740', 'Iwan Erpan', 'Laki-Laki', 'iwan.erpan@indonesiaferry.co.id', 'Pelayanan TI', '2009-01-05', 'face1.jpg', '', '-- belum terdata --'),
-(2, '082134536', 'Dennish Candra', 'Laki-Laki', 'dennis.kusuma@indonesiaferry.co.id', 'Pengembangan TI', '2013-08-26', 'face21.jpg', 'HO', '-- belum terdata --'),
-(4, '82134534', 'Agus Lutfiyanto', 'Laki-Laki', 'lutfi.agus@indonesiaferry.co.id', 'Operasional TI', '2013-08-26', 'face8.jpg', '-- belum terdata --', '-- belum terdata --'),
-(5, 'KKP_852', 'Muhammad Sholeh', 'Laki-Laki', 'muhammad.sholeh@indonesiaferry.co.id', 'Operasional TI', '2024-07-18', 'sholeh.jpg', '', 'cuki'),
-(8, 'KKP_857', 'Adriansyah Prasetya', 'Laki-Laki', 'adriansyahprasetyanugraha1@gmail.com', 'Operasional TI', '2024-08-07', 'default.png', 'HO', '-- belum terdata --'),
-(9, 'KKP_847', 'Iqbal', 'Laki-Laki', 'iqbal@asdp.id', 'Pengembangan TI', '2024-07-17', 'pas photo.jpg', 'HO', 'kos sebelah'),
-(10, 'KKP_111', 'Abdillah', 'Laki-Laki', 'abdillah@gmail.com', 'Perencanaan TI', '2001-01-01', 'pas photo.jpg', '', '-- belum terdata --');
 
 -- --------------------------------------------------------
 
@@ -82,7 +77,7 @@ INSERT INTO `karyawan` (`id`, `nik`, `nama_karyawan`, `jenis_kelamin`, `email`, 
 CREATE TABLE `login` (
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `role` enum('admin','karyawan') NOT NULL
+  `role` enum('admin','penempatan') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -91,9 +86,6 @@ CREATE TABLE `login` (
 
 INSERT INTO `login` (`email`, `password`, `role`) VALUES
 ('admin@indonesiaferry.co.id', '$2a$12$L2Kr2uxAkWaK1qyepoGWaON.6SoETIDXhngOX9WgoXWOYJUGWW6zy', 'admin'),
-('lutfi.agus@indonesiaferry.co.id', '$2a$12$L2Kr2uxAkWaK1qyepoGWaON.6SoETIDXhngOX9WgoXWOYJUGWW6zy', 'karyawan'),
-('dennis.kusuma@indonesiaferry.co.id', '$2a$12$L2Kr2uxAkWaK1qyepoGWaON.6SoETIDXhngOX9WgoXWOYJUGWW6zy', 'karyawan'),
-('muhammad.sholeh@indonesiaferry.co.id', '$2a$12$L2Kr2uxAkWaK1qyepoGWaON.6SoETIDXhngOX9WgoXWOYJUGWW6zy', 'karyawan');
 
 --
 -- Indexes for dumped tables
@@ -107,11 +99,11 @@ ALTER TABLE `admin`
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- Indeks untuk tabel `karyawan`
+-- Indeks untuk tabel `penempatan`
 --
-ALTER TABLE `karyawan`
+ALTER TABLE `penempatan`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `nik` (`nik`);
+  ADD UNIQUE KEY `kode_aset` (`kode_aset`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
@@ -124,10 +116,11 @@ ALTER TABLE `admin`
   MODIFY `nomor` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT untuk tabel `karyawan`
+-- AUTO_INCREMENT untuk tabel `penempatan`
 --
-ALTER TABLE `karyawan`
+ALTER TABLE `penempatan`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
